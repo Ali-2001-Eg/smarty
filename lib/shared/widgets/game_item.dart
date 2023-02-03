b4cb816de65919dea8d39652785f6a3010705034
Ali-2001-Eg <AliBusiness2001@outlook.com>
2023-02-03 23:22:29 +0200

    game levels

--- lib/shared/widgets/game_item.dart
+++ lib/shared/widgets/game_item.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 class GameItems extends StatelessWidget {
   final String imagePath;
   final String title;
-  final String url;
-  GameItems(this.imagePath, this.title,this.url);
+
+  GameItems(this.imagePath, this.title,);
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+
